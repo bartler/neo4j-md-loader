a68e7b7b37f4b89e5f429fa7b23fa50285bc3756
Abe Raher <abe@cribl.io>
2023-06-21 02:14:32 -0700

    add fields to the Release object

--- src/release.ts
+++ src/release.ts
@@ -1,11 +1,21 @@
 class Release {
     //types: 
-    // Major, e.g. 3.x -- consider for future, ignore for now
-    // Feature, e.g. 3.2.x
+    // Major, e.g. 3.0.0
+    // MajorParent, e.g. 3.x
+    // Feature, e.g. 3.2.0
+    // FeatureParent, e.g. 3.2.x
     // Maintenance, e.g. 3.2.1
+    // product:
+    // for now, always Stream
+    // in future, we'll generate Release nodes for
+    // Stream, Edge, and Search ... should we make this an array?
+    // datePlain -- e.g., "2022-09-15"
+    // dateUTC -- e.g., "2023-06-14T09:32:41.070Z"
     type: String  
     version: String[]
-    releaseDate: String
+    product: String
+    datePlain: String
+    dateUTC: String
     constructor() {
     }
 }
